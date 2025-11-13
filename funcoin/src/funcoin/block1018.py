bdf533ef1783f1cdc8ed5069372a443adeb4822d
Ivan <Ivanjuliusoliver@gmail.com>
2025-11-12 21:59:23 -0600

    Created a dummy block.

---
+++ funcoin/src/funcoin/block1018.py
@@ -0,0 +1,13 @@
+block_1018 = {
+    'index': 1018,
+    'timestamp': "2025-11-12T08:07:42.170675",
+    'data': [
+        {
+            'sender': "bob",
+            'recipient': "alice",
+            'amount': "$5",
+        }
+    ],
+    'hash': "83b2ac5b",
+    'previous_hash': "2cf24ba5f"
+}
